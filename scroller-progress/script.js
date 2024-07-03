4ed19ed0b93d890831e76a19ab2ebc95497c4d64
jatwvv <146978372+jatwvv@users.noreply.github.com>
2024-07-03 20:42:40 +0300

    learned scroller

---
+++ scroller-progress/script.js
@@ -0,0 +1,11 @@
+// scrollHeight is the entire content and padding
+//client height is the visible content and padding
+
+let el = document.querySelector(".scroller");
+let height =
+  document.documentElement.scrollHeight - document.documentElement.clientHeight;
+
+window.addEventListener("scroll", () => {
+  let scrollTop = document.documentElement.scrollTop;
+  el.style.width = `${(scrollTop / height) * 100}%`;
+});
